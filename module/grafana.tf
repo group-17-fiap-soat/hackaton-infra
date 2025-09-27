eb3f1df4234f7d358768a5f6ed26bd5680a1496d
Manocchio <guruu.leste@gmail.com>
2025-09-27 20:18:16 -0300

    another config for grafana

--- module/grafana.tf
+++ module/grafana.tf
@@ -1,48 +1,98 @@
-resource "helm_release" "prometheus" {
-  name              = "prometheus-min"
-  repository        = "https://prometheus-community.github.io/helm-charts"
-  chart             = "prometheus"
-  namespace         = "default"
-  create_namespace  = false
-  wait              = false
-  timeout           = 1200
-  replace           = true
-  cleanup_on_fail   = true
-  dependency_update = true
-
-  values = [<<EOF
-alertmanager:
-  enabled: false
-pushgateway:
-  enabled: false
-kubeStateMetrics:
-  enabled: false
-nodeExporter:
-  enabled: false
-server:
-  persistentVolume:
-    enabled: false
-  service:
-    type: ClusterIP
-rbac:
-  create: false
+resource "kubernetes_config_map" "prometheus_config" {
+  metadata {
+    name      = "prometheus-config"
+    namespace = "default"
+  }
+  data = {
+    "prometheus.yml" = <<EOF
+global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: "prometheus"
+    static_configs:
+      - targets: ["prometheus-server.default.svc.cluster.local:9090"]
 EOF
-  ]
+  }
+}
+
+resource "kubernetes_deployment" "prometheus" {
+  metadata {
+    name      = "prometheus"
+    namespace = "default"
+    labels = {
+      app = "prometheus"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "prometheus"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "prometheus"
+        }
+      }
+      spec {
+        container {
+          name  = "prometheus"
+          image = "prom/prometheus:v2.53.0"
+          args  = ["--config.file=/etc/prometheus/prometheus.yml", "--storage.tsdb.path=/prometheus"]
+          port {
+            container_port = 9090
+          }
+          volume_mount {
+            name       = "config"
+            mount_path = "/etc/prometheus"
+          }
+        }
+        volume {
+          name = "config"
+          config_map {
+            name = kubernetes_config_map.prometheus_config.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "prometheus_server" {
+  metadata {
+    name      = "prometheus-server"
+    namespace = "default"
+    labels = {
+      app = "prometheus"
+    }
+  }
+  spec {
+    selector = {
+      app = "prometheus"
+    }
+    port {
+      name        = "http"
+      port        = 9090
+      target_port = 9090
+    }
+    type = "ClusterIP"
+  }
 }
 
 resource "helm_release" "grafana" {
-  depends_on        = [helm_release.prometheus]
-  name              = "grafana"
-  repository        = "https://grafana.github.io/helm-charts"
-  chart             = "grafana"
-  namespace         = "default"
-  create_namespace  = false
-  wait              = false
-  timeout           = 1800
-  replace           = true
-  cleanup_on_fail   = true
-  dependency_update = true
-
+  depends_on         = [kubernetes_service.prometheus_server]
+  name               = "grafana"
+  repository         = "https://grafana.github.io/helm-charts"
+  chart              = "grafana"
+  namespace          = "default"
+  create_namespace   = false
+  wait               = false
+  timeout            = 1800
+  replace            = true
+  cleanup_on_fail    = true
+  dependency_update  = true
   values = [<<EOF
 adminUser: admin
 adminPassword: admin
