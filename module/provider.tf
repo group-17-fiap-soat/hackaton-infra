15811ebde64781d3bce6093b3d450b76f4582622
Manocchio <guruu.leste@gmail.com>
2025-09-27 19:27:37 -0300

    fix providers

--- module/provider.tf
+++ module/provider.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    config_path = "~/.kube/config"
   }
 }
 
@@ -41,12 +42,4 @@ terraform {
   }
 
   required_version = ">= 1.5"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
 }
