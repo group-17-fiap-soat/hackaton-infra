be71f6c3b486345cce934d87672209f948842da5
Manocchio <guruu.leste@gmail.com>
2025-09-27 15:16:38 -0300

    Revert "add rds"
    This reverts commit b7ad29b987dd2fe2914e126b0edd0871a8b31bd5.

--- module/variables.tf
+++ module/variables.tf
@@ -67,10 +67,3 @@ variable "sonar_key_name" {
   type        = string
   default     = "devkeypair"
 }
-
-variable "db_name" {}
-variable "db_user" {}
-variable "db_password" {}
-variable "db_port" {
-  default = "5432"
-}
