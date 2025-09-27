60470f2bf3f32a8e784c8d4953d181b237f91821
Manocchio <guruu.leste@gmail.com>
2025-09-27 20:14:45 -0300

    add prometheus helm release and update grafana configuration

--- module/grafana.tf
+++ module/grafana.tf
@@ -1,45 +1,68 @@
-resource "helm_release" "grafana" {
-  name             = "grafana-monitoring"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "grafana"
-  namespace        = "default"
-
-  wait    = false
-  timeout = 1800
-
-  replace    = true
+resource "helm_release" "prometheus" {
+  name              = "prometheus-min"
+  repository        = "https://prometheus-community.github.io/helm-charts"
+  chart             = "prometheus"
+  namespace         = "default"
+  create_namespace  = false
+  wait              = false
+  timeout           = 1200
+  replace           = true
   cleanup_on_fail   = true
   dependency_update = true
-  create_namespace = false
 
   values = [<<EOF
-adminUser: admin
-adminPassword: admin
-persistence:
-  enabled: true
-  size: 5Gi
-service:
-  type: LoadBalancer
+alertmanager:
+  enabled: false
+pushgateway:
+  enabled: false
+kubeStateMetrics:
+  enabled: false
+nodeExporter:
+  enabled: false
+server:
+  persistentVolume:
+    enabled: false
+  service:
+    type: ClusterIP
 rbac:
   create: false
 EOF
   ]
 }
 
-resource "grafana_data_source" "prometheus" {
-  depends_on  = [helm_release.grafana]
-  name        = "Prometheus"
-  type        = "prometheus"
-  url         = "http://prometheus-server.monitoring.svc.cluster.local"
-  access_mode = "proxy"
-  is_default  = true
-}
+resource "helm_release" "grafana" {
+  depends_on        = [helm_release.prometheus]
+  name              = "grafana"
+  repository        = "https://grafana.github.io/helm-charts"
+  chart             = "grafana"
+  namespace         = "default"
+  create_namespace  = false
+  wait              = false
+  timeout           = 1800
+  replace           = true
+  cleanup_on_fail   = true
+  dependency_update = true
 
-data "http" "node_dash" {
-  url = "https://grafana.com/api/dashboards/1860/revisions/32/download"
-}
-
-resource "grafana_dashboard" "nodes" {
-  depends_on  = [grafana_data_source.prometheus]
-  config_json = data.http.node_dash.response_body
+  values = [<<EOF
+adminUser: admin
+adminPassword: admin
+persistence:
+  enabled: false
+rbac:
+  create: false
+service:
+  type: LoadBalancer
+serviceAccount:
+  create: false
+datasources:
+  datasources.yaml:
+    apiVersion: 1
+    datasources:
+      - name: Prometheus
+        type: prometheus
+        url: http://prometheus-server.default.svc.cluster.local
+        access: proxy
+        isDefault: true
+EOF
+  ]
 }
