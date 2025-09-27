b861e5f03e859e0610c774acf4eb8f2647cbaab5
Manocchio <guruu.leste@gmail.com>
2025-09-27 19:21:17 -0300

    fix provider.tf

--- module/grafana.tf
+++ module/grafana.tf
@@ -1,38 +1,5 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">=2.9.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.24.0"
-    }
-    grafana = {
-      source  = "grafana/grafana"
-      version = ">=2.0.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">=3.0.0"
-    }
-  }
-}
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-provider "grafana" {
-  url  = "http://grafana.monitoring.svc.cluster.local"
-  auth = "admin:admin"
-}
 
 resource "helm_release" "grafana" {
   name       = "grafana"
--- module/provider.tf
+++ module/provider.tf
@@ -2,13 +2,46 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "grafana" {
+  url  = "http://grafana.monitoring.svc.cluster.local"
+  auth = "admin:admin"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.24.0"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = ">=2.0.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">=3.0.0"
+    }
   }
 
   required_version = ">= 1.5"
 }
+
